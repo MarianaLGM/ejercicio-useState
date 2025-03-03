880f1496b86f19ca5a031a6386f4abcd28d31b8d
MarianaLGM <mariana_lobeto@hotmail.com>
2025-03-03 13:10:04 +0100

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import './App.css'
+import { useState } from 'react'; //1- importamos `useState` desde React.
+
 
 function App() {
- //Aquí tu código
+ //2.definimos varibles estado name y newname
+const [name, setName]= useState("Sofía");// Nombre actual del profesor
+const [newName, setNewName]= useState("");// Nuevo nombre profe ingresado por el usuario
+
+return (
+    //3. renderiza un título `<h2>` con el texto "Teacher Name" seguido del valor de `name`.
+    //4. Renderiza una lista `<ul>` con tres elementos `<li>`
+    //5. Utilizamos `setName` proporcionada por `useState` para actualizar el nombre del profesor cuando se haga clic en un elemento de la lista.
+    <div>
+        <h2>Teacher Name {name}</h2> 
+        <ul>
+            <li onClick={() => setName ("Data")}>Data</li>
+            <li onClick={() => setName ("Reyes")}>Reyes</li>
+            <li onClick={() => setName ("Yolanda")}>Yolanda</li>
+        </ul>
+
+    </div> 
+    )
 }
 
 export default App
